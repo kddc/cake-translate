5c67d80d25c5833b3106d32151cb6fa79c21396f
m3daze <m3daze@gmail.com>
2016-02-13 18:00:26 +0100

    Hauptseite gestyled

--- www/js/app.js
+++ www/js/app.js
@@ -39,6 +39,8 @@ angular.module('cake-translate', ['ionic', 'ngCordova'])
 		$scope.$apply(function() {
 			$scope.cordovaReady = true;
 			$scope.window = window;
+
+      $scope.loadAllImageWordsPairs();
 		});
 	});
 
